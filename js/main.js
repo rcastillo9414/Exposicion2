4bb6ba01ebf8862e447aa7af0c7eaa957215e539
rcastillo9414 <rcastillom@ucenfotec.ac.cr>
2025-10-23 18:00:59 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Año en el footer
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Formularios validacion
+(() => {
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
+
